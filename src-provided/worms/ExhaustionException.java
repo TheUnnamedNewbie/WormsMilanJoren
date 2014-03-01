130f215d8fa2806ca915081824639da33c9a6a3f
Joren Vaes <joren.vaes@gmail.com>
2014-03-01 16:59:50 +0100

    Added ExhaustionException

---
+++ src-provided/worms/ExhaustionException.java
@@ -0,0 +1,14 @@
+package worms;
+
+public class ExhaustionException extends RuntimeException {
+
+	/**
+	 * You cannot do action asked because you do not have sufficient action points
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ExhaustionException() {
+		
+	}
+
+}
