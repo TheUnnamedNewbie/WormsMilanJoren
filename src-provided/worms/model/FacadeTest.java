f1307548fa484feb1305074c7766eb4e6bccd443
Milan <milansanders95@gmail.com>
2014-03-08 13:36:06 +0100

    Correctly called the interface IFacade and changed the spacing on assertEquals(double, double, spacing) to DEFAULT_EPSILON in java.util

--- src-provided/worms/model/FacadeTest.java
+++ src-provided/worms/model/FacadeTest.java
@@ -5,42 +5,44 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import worms.ExhaustionException;
+import worms.util.Util;
 
-public abstract class FacadeTest {
-	public abstract Facade createFacade();
-	Facade instance = createFacade();
+public class FacadeTest {
+	private static final double EPS = Util.DEFAULT_EPSILON;
+
+	private IFacade facade;
 
 	@Test
 	public void testCreation() {
 		Worm worm1 = new Worm("James May", 0, 0, 0, 1);
-		Worm worm2 = instance.createWorm(0, 0, 0, 1, "James May");
-		assertEquals(worm1.getPosX(), worm2.getPosX(), 0.00001);
-		assertEquals(worm1.getPosY(), worm2.getPosY(), 0.00001);
-		assertEquals(worm1.getOrientation(), worm2.getOrientation(), 0.00001);
-		assertEquals(worm1.getRadius(), worm2.getRadius(), 0.00001);
-		assertEquals(worm1.getMass(), worm2.getMass(), 0.00001);
+		Worm worm2 = facade.createWorm(0, 0, 0, 1, "James May");
+		assertEquals(worm1.getPosX(), worm2.getPosX(), EPS);
+		assertEquals(worm1.getPosY(), worm2.getPosY(),EPS);
+		assertEquals(worm1.getOrientation(), worm2.getOrientation(), EPS);
+		assertEquals(worm1.getRadius(), worm2.getRadius(), EPS);
+		assertEquals(worm1.getMass(), worm2.getMass(), EPS);
 		assertEquals(worm1.getName(), worm2.getName());
 	}
 	
 	@Test
 	public void testGetters() {
 		Worm worm1 = new Worm("James May", 0, 0, 0, 1);
-		assertEquals(worm1.getPosX(), instance.getX(worm1), 0.00001);
-		assertEquals(worm1.getPosY(), instance.getY(worm1), 0.00001);
-		assertEquals(worm1.getOrientation(), instance.getOrientation(worm1), 0.00001);
-		assertEquals(worm1.getRadius(), instance.getRadius(worm1), 0.00001);
-		assertEquals(worm1.getMass(), instance.getMass(worm1), 0.00001);
-		assertEquals(worm1.getName(), instance.getName(worm1));
-		assertEquals(0.25, instance.getMinimalRadius(worm1), 0.00001);
-		assertEquals(worm1.getActionPoints(), instance.getActionPoints(worm1));
-		assertEquals(worm1.getMaxActionPoints(), instance.getMaxActionPoints(worm1));
-		assertEquals(worm1.getJumpTime(), instance.getJumpTime(worm1), 0.00001);
+		assertEquals(worm1.getPosX(), facade.getX(worm1), EPS);
+		assertEquals(worm1.getPosY(), facade.getY(worm1), EPS);
+		assertEquals(worm1.getOrientation(), facade.getOrientation(worm1), EPS);
+		assertEquals(worm1.getRadius(), facade.getRadius(worm1), EPS);
+		assertEquals(worm1.getMass(), facade.getMass(worm1), EPS);
+		assertEquals(worm1.getName(), facade.getName(worm1));
+		assertEquals(0.25, facade.getMinimalRadius(worm1), EPS);
+		assertEquals(worm1.getActionPoints(), facade.getActionPoints(worm1));
+		assertEquals(worm1.getMaxActionPoints(), facade.getMaxActionPoints(worm1));
+		assertEquals(worm1.getJumpTime(), facade.getJumpTime(worm1), EPS);
 	}
 	
 	@Test
 	public void testMoveLegal() {
 		Worm worm1 = new Worm("James May", 0, 0, 0, 1);
-		instance.move(worm1, 2);
+		facade.move(worm1, 2);
 		assertEquals(worm1.getPosX(), 2.0, 0.00001);
 		int targetAP = Worm.roundUp(worm1.getActionPoints() - Math.abs(Math.cos(0)) - Math.abs(4 * Math.sin(0)));
 		assertEquals(targetAP, worm1.getActionPoints());
@@ -49,13 +51,13 @@ public abstract class FacadeTest {
 	@Test(expected = IllegalArgumentException.class)
 	public void testMoveIllegalArg() {
 		Worm worm1 = new Worm("James May", 0, 0, 0, 1);
-		instance.move(worm1, -2);
+		facade.move(worm1, -2);
 	}
 	
 	@Test
 	public void testTurnLegal() {
 		Worm worm1 = new Worm("James May", 0, 0, 0, 1);
-		instance.turn(worm1, Math.PI);
+		facade.turn(worm1, Math.PI);
 		assertEquals(Math.PI, worm1.getOrientation(), 0.00001);
 		int targetAP = Worm.roundUp(worm1.getActionPoints() - (Math.abs(Math.PI) / (2 * Math.PI)) * 60);
 		assertEquals(targetAP, worm1.getActionPoints());
@@ -64,53 +66,53 @@ public abstract class FacadeTest {
 	@Test
 	public void testJump() {
 		Worm worm1 = new Worm("James May", 0, 0, 0, 1);
-		instance.turn(worm1, Math.PI/4.0);
-		instance.jump(worm1);
+		facade.turn(worm1, Math.PI/4.0);
+		facade.jump(worm1);
 		assertEquals(0,worm1.getActionPoints());
 		double targetJumpX = Math.cos(Math.PI/4.0)*(1+1/9.80665)*((5*worm1.getMaxActionPoints()+9.80665*worm1.getMass())/worm1.getMass());
-		assertEquals(targetJumpX, worm1.getPosX(), 0.00001);
-		assertEquals(0.0, worm1.getPosY(), 0.00001);
+		assertEquals(targetJumpX, worm1.getPosX(), EPS);
+		assertEquals(0.0, worm1.getPosY(), EPS);
 	}
 	
 	@Test
 	public void testSetRadiusLegal() {
 		Worm worm1 = new Worm("James May", 0, 0, 0, 1);
-		instance.setRadius(worm1, 2.0);
-		assertEquals(2.0, worm1.getRadius(), 0.00001);
-		assertEquals(1062 * ((4 / 3) * Math.PI * Math.pow(2.0 , 3)), worm1.getMass(), 0.00001);
+		facade.setRadius(worm1, 2.0);
+		assertEquals(2.0, worm1.getRadius(), EPS);
+		assertEquals(1062 * ((4 / 3) * Math.PI * Math.pow(2.0 , 3)), worm1.getMass(), EPS);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testSetRadiusIllegal() {
 		Worm worm1 = new Worm("James May", 0, 0, 0, 1);
-		instance.setRadius(worm1, -1.0);
+		facade.setRadius(worm1, -1.0);
 	}
 	
 	@Test
 	public void testRenameLegal() {
 		Worm worm1 = new Worm("James May", 0, 0, 0, 1);
-		instance.rename(worm1, "Captain Slow");
+		facade.rename(worm1, "Captain Slow");
 		assertEquals(worm1.getName(), "Captain Slow");
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testRenameIllegal() {
 		Worm worm1 = new Worm("James May", 0, 0, 0, 1);
-		instance.rename(worm1, "James & Jeremy");
+		facade.rename(worm1, "James & Jeremy");
 	}
 	
 	@Test(expected = ExhaustionException.class)
 	public void testExhaustedMove() {
 		Worm worm1 = new Worm("James May", 0, 0, 0, 1);
-		instance.jump(worm1);
-		instance.move(worm1, 1);
+		facade.jump(worm1);
+		facade.move(worm1, 1);
 	}
 	
 	@Test(expected = ExhaustionException.class)
 	public void testExhaustedJump() {
 		Worm worm1 = new Worm("James May", 0, 0, 0, 1);
-		instance.jump(worm1);
-		instance.jump(worm1);
+		facade.jump(worm1);
+		facade.jump(worm1);
 	}
 
 }
