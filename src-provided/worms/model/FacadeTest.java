153d9788543b339afb45da0fe8b868ec5ac2b97f
Milan <milansanders95@gmail.com>
2014-03-08 09:39:10 +0100

    Created TestFacade class

---
+++ src-provided/worms/model/FacadeTest.java
@@ -0,0 +1,104 @@
+package worms.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FacadeTest {
+
+	@Test
+	public void testCreateWorm() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanTurn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTurn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJump() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetJumpTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetJumpStep() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOrientation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRadius() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRadius() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMinimalRadius() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetActionPoints() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMaxActionPoints() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRename() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMass() {
+		fail("Not yet implemented");
+	}
+
+}
