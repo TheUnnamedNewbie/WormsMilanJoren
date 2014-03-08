b0b089a8c095d5f33f4e1c0bc397abb54380d15c
Milan <milansanders95@gmail.com>
2014-03-08 10:58:16 +0100

    Retry the TestFacade, shit got weird and I panicked

---
+++ src-provided/worms/model/FacadeTest.java
@@ -0,0 +1,14 @@
+package worms.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FacadeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
