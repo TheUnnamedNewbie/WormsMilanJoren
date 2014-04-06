af434ecaba363c3b2f2673c7b6c65ea63df97593
Milan <milansanders95@gmail.com>
2014-04-06 16:33:14 +0200

    Generated stub implementations of IFacade

--- src/worms/model/Facade.java
+++ src/worms/model/Facade.java
@@ -1,5 +1,8 @@
 package worms.model;
 
+import java.util.Collection;
+import java.util.Random;
+
 import worms.ExhaustionException;
 
 /**
@@ -108,6 +111,248 @@ public class Facade implements IFacade {
 	
 	public double getMass(Worm worm) {
 		return worm.getMass();
+	}
+
+	@Override
+	public void addEmptyTeam(World world, String newName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addNewFood(World world) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addNewWorm(World world) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean canFall(Worm worm) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canMove(Worm worm) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Food createFood(World world, double x, double y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public World createWorld(double width, double height,
+			boolean[][] passableMap, Random random) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Worm createWorm(World world, double x, double y, double direction,
+			double radius, String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void fall(Worm worm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Projectile getActiveProjectile(World world) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Worm getCurrentWorm(World world) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Food> getFood(World world) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getHitPoints(Worm worm) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double[] getJumpStep(Projectile projectile, double t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getJumpTime(Projectile projectile, double timeStep) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getJumpTime(Worm worm, double timeStep) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMaxHitPoints(Worm worm) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getRadius(Food food) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getRadius(Projectile projectile) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getSelectedWeapon(Worm worm) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTeamName(Worm worm) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getWinner(World world) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Worm> getWorms(World world) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getX(Food food) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getX(Projectile projectile) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getY(Food food) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getY(Projectile projectile) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isActive(Food food) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isActive(Projectile projectile) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAdjacent(World world, double x, double y, double radius) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAlive(Worm worm) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isGameFinished(World world) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isImpassable(World world, double x, double y, double radius) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void jump(Projectile projectile, double timeStep) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void jump(Worm worm, double timeStep) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move(Worm worm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selectNextWeapon(Worm worm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void shoot(Worm worm, int yield) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startGame(World world) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startNextTurn(World world) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
